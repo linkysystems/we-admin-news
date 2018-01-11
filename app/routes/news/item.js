78b990f755d298f9e5ea21f8d95b789bfe5aa436
albertosouza <alberto.souza.99@gmail.com>
2018-01-11 18:55:57 -0200

    add news templates and routes

---
+++ app/routes/news/item.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  term: Ember.inject.service(),
+  model(params) {
+    return Ember.RSVP.hash({
+      record: this.get('store').findRecord('news', params.id),
+      categories: this.get('term').getSystemCategories(),
+      alias: this.get('store').query('url-alia', {
+        target: '/news/'+params.id,
+        limit: 1,
+        order: 'id DESC'
+      })
+      .then( (r)=> { // get only one alias:
+        if (r && r.objectAt && r.objectAt(0)) {
+          return r.objectAt(0);
+        } else {
+          return null;
+        }
+      }),
+    });
+  },
+  afterModel(model) {
+    if (
+      model.alias && model.alias.alias && model.record && model.record.id
+    ) {
+      Ember.set(model.record, 'setAlias', Ember.get(model.alias,'alias'));
+    } else {
+      model.alias = this.get('store').createRecord('url-alia', {
+        target: '/news/'+id,
+        alias: '/noticias/'+id
+      });
+    }
+  }
+});
